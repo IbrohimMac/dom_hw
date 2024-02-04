e2b87fe045076b0402e7923af8334afcc6b86828
Xpanetel <ibrohim@MAC.local>
2024-02-04 23:22:35 +0500

    nnn

---
+++ js/home.js
@@ -0,0 +1,33 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+const btnOpen = document.getElementById("btn-open");
+const btnClose = document.getElementById("btn-close");
+const modal = document.querySelector(".modal");
+
+btnOpen.addEventListener("click", function (e) {
+  modal.classList.add("open");
+});
+
+btnClose.addEventListener("click", function (e) {
+  modal.classList.remove("open");
+});
